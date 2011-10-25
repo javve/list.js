49b08872f0cc9a620b277ef28d70c6ac84d642e7
Jonny Strömberg <jonny.stromberg@gmail.com>
2011-10-25 22:34:56 +0200

    Negative numbers sorting bug fix by bwanab

--- src/list.js
+++ src/list.js
@@ -280,7 +280,7 @@ function List(id, options, values) {
             var tz = [], x = 0, y = -1, n = 0, i, j;
 
             while (i = (j = t.charAt(x++)).charCodeAt(0)) {
-                var m = (i == 46 || (i >=48 && i <= 57));
+                var m = (i == 45 || i == 46 || (i >=48 && i <= 57));
                 if (m !== n) {
                     tz[++y] = "";
                     n = m;
