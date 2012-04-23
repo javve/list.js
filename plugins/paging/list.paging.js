b2e418650f7318a427c78daf6f0c2bb0aeb07a65
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-04-23 10:30:28 +0200

    Remove some whitespace

--- plugins/paging/list.paging.js
+++ plugins/paging/list.paging.js
@@ -1,6 +1,6 @@
 List.prototype.plugins.paging = function(list, options) {
     var pagingList;
-    
+
     var init = function() {
         options = options || {};
         pagingList = new List(list.listContainer.id, {
@@ -13,7 +13,7 @@ List.prototype.plugins.paging = function(list, options) {
         list.on('updated', refresh);
         refresh();
     };
-    
+
     var refresh = function() {
         var l = list.matchingItems.length,
             index = list.i,
@@ -24,14 +24,14 @@ List.prototype.plugins.paging = function(list, options) {
             left = options.left || options.outerWindow || 0,
             right = options.right || options.outerWindow || 0,
             right = pages - right;
-            
+
         pagingList.clear();
-        
+
         for (var i = 1; i <= pages; i++) {
             var className = (currentPage === i) ? "active" : "";
-            
+
             //console.log(i, left, right, currentPage, (currentPage - innerWindow), (currentPage + innerWindow));
-            
+
             if (is.number(i, left, right, currentPage, innerWindow)) {
                 var item = pagingList.add({
                     page: "<a class='"+className+"' href='javascript:function Z(){Z=\"\"}Z()'>"+(i)+"</a>",
@@ -46,14 +46,14 @@ List.prototype.plugins.paging = function(list, options) {
             }
         }
     };
-    
+
     var is = {
         number: function(i, left, right, currentPage, innerWindow) {
            return this.left(i, left) || this.right(i, right) || this.innerWindow(i, currentPage, innerWindow);
         },
         left: function(i, left) {
             return (i <= left);
-        }, 
+        },
         right: function(i, right) {
             return (i > right);
         },
@@ -61,7 +61,7 @@ List.prototype.plugins.paging = function(list, options) {
             return ( i >= (currentPage - innerWindow) && i <= (currentPage + innerWindow));
         },
         dotted: function(i, left, right, currentPage, innerWindow, currentPageItem) {
-            return this.dottedLeft(i, left, right, currentPage, innerWindow) 
+            return this.dottedLeft(i, left, right, currentPage, innerWindow)
             || (this.dottedRight(i, left, right, currentPage, innerWindow, currentPageItem));
         },
         dottedLeft: function(i, left, right, currentPage, innerWindow) {
@@ -69,19 +69,19 @@ List.prototype.plugins.paging = function(list, options) {
         },
         dottedRight: function(i, left, right, currentPage, innerWindow, currentPageItem) {
             if (pagingList.items[currentPageItem-1].values().dotted) {
-                return false 
+                return false
             } else {
                 return ((i == (right)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right))
             }
         }
     };
-    
+
     var addEvent = function(elm, i, page) {
        ListJsHelpers.addEvent(elm, 'click', function() {
            list.show((i-1)*page + 1, page);
        });
     };
-    
+
     init();
     return this;
 };
