22b7477a716be500bb8866f9b2cecc8fcbc78c52
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-09-18 14:13:33 +0200

    Tests for .trigger

---
+++ test/test.trigger.js
@@ -0,0 +1,21 @@
+describe('Trigger', function() {
+
+    var list;
+
+    before(function() {
+        list = fixture.list(['name', 'born'], fixture.all);
+    });
+
+    after(function() {
+        fixture.removeList();
+    });
+
+    describe('General', function() {
+        it('should be triggered by searchComplete', function(done) {
+            list.on('searchComplete', function() {
+                done();
+            });
+            list.trigger('searchComplete');
+        });
+    });
+});
