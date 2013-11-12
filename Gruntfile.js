23a8dc7070bfba60d0decc8988def5f7c2429434
Jonny Strömberg <jonny.stromberg@gmail.com>
2013-11-12 18:14:45 +0100

    Replace Make with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: require("./package.json"),
+    watch: {
+      options: {}
+    },
+    shell: {
+      install: {
+        command: 'component install --dev'
+      },
+      build: {
+        command: 'component build --dev'
+      },
+      standalone: {
+        command: 'component build --standalone List -n list.standalone'
+      },
+      mkdir: {
+        command: 'mkdir -p dist'
+      },
+      move: {
+        command: 'mv build/list.standalone.js dist/list.js'
+      },
+      remove: {
+        command: 'rm -fr build components dist'
+      }
+    },
+    uglify: {
+      target: {
+        files: {
+          'dist/list.min.js': ['dist/list.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['shell:install', 'shell:build']);
+  grunt.registerTask('dist', ['default', 'shell:standalone', 'shell:mkdir', 'shell:move', 'uglify']);
+  grunt.registerTask('clean', ['shell:remove']);
+
+  return grunt;
+};
