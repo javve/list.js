42023bfcda5f3044cfd94a99d64f9554be6b957d
Jeremy S <jeresiv@gmail.com>
2014-08-05 16:13:37 -0400

    Modified templater.set to allow for input lists.

--- src/templater.js
+++ src/templater.js
@@ -47,6 +47,8 @@ var Templater = function(list) {
                         /* src attribute for image tag & text for other tags */
                         if (elm.tagName === "IMG" && values[v] !== "") {
                             elm.src = values[v];
+						} else if (elm.tagName === "INPUT" && elm.hasAttribute("value")) {
+							elm.value = values[v];
                         } else {
                             elm.innerHTML = values[v];
                         }
