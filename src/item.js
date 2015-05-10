8e898b0e55a7d47a77ee27f109602bdb63183fda
Jonny Strömberg <jonny.stromberg@gmail.com>
2015-05-10 21:57:49 +0200

    Fix #222

--- src/item.js
+++ src/item.js
@@ -47,7 +47,7 @@ module.exports = function(list) {
       );
     };
     this.visible = function() {
-      return (item.elm.parentNode == list.list) ? true : false;
+      return (item.elm && (item.elm.parentNode == list.list)) ? true : false;
     };
     init(initValues, element, notCreate);
   };
