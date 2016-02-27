6e3a6ea9c70718624859a2a4a9ec3ff0b37873d4
Jonny Strömberg <jonny.stromberg@gmail.com>
2016-02-27 14:13:48 +0100

    Remove code that doesn’t work and aren’t used

--- index.js
+++ index.js
@@ -126,13 +126,8 @@ var List = function(id, options, values) {
     }
     for (var i = 0, il = values.length; i < il; i++) {
       var item = null;
-      if (values[i] instanceof Item) {
-        item = values[i];
-        item.reload();
-      } else {
-        notCreate = (self.items.length > self.page) ? true : false;
-        item = new Item(values[i], undefined, notCreate);
-      }
+      notCreate = (self.items.length > self.page) ? true : false;
+      item = new Item(values[i], undefined, notCreate);
       self.items.push(item);
       added.push(item);
     }
