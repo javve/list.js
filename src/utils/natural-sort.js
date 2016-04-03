0d8caf8107a44b3a5f60b1e43ddf999bd01e05da
Christopher Beloch <webmaster@cbeloch.de>
2016-04-03 03:31:39 +0200

    Update natural-sort.js
    fix javve/list.js#385

--- src/utils/natural-sort.js
+++ src/utils/natural-sort.js
@@ -23,8 +23,11 @@ module.exports = function(a, b, opts) {
         xD = parseInt(x.match(hre), 16) || (xN.length !== 1 && Date.parse(x)),
         yD = parseInt(y.match(hre), 16) || xD && y.match(dre) && Date.parse(y) || null,
         normChunk = function(s, l) {
-           // normalize spaces; find floats not starting with '0', string or 0 if not defined (Clint Priest)
-           return (!s.match(ore) || l == 1) && parseFloat(s) || s.replace(snre, ' ').replace(sre, '') || 0;
+            if (!s) {
+                return 0;
+            }
+            // normalize spaces; find floats not starting with '0', string or 0 if not defined (Clint Priest)
+            return (!s.match(ore) || l == 1) && parseFloat(s) || s.replace(snre, ' ').replace(sre, '') || 0;
         },
         oFxNcL, oFyNcL;
    // first try and sort Hex codes or Dates
