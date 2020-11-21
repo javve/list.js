572cc9519a3c84fe7344ff5c88e94c9889a12e21
Jonny Strömberg <jonny.stromberg@gmail.com>
2020-11-21 22:28:00 +0100

    Revert to old fuzzy search

--- src/utils/fuzzy.js
+++ src/utils/fuzzy.js
@@ -11,20 +11,20 @@ module.exports = function (text, pattern, options) {
   if (pattern === text) return true // Exact match
   if (pattern.length > 32) return false // This algorithm cannot be used
 
-    // Set starting location at beginning text and initialise the alphabet.
-    var loc = Match_Location,
-        s = (function() {
-            var q = {},
-                i,
-                il;
+  // Set starting location at beginning text and initialise the alphabet.
+  var loc = Match_Location,
+    s = (function () {
+      var q = {},
+        i
 
-            for (i = 0, il = pattern.length; i < il; i++) {
-                q[pattern.charAt(i)] = 0;
-            }
+      for (i = 0; i < pattern.length; i++) {
+        q[pattern.charAt(i)] = 0
+      }
 
-            for (i = 0, il = pattern.length; i < il; i++) {
-                q[pattern.charAt(i)] |= 1 << (il - i - 1);
-            }
+      for (i = 0; i < pattern.length; i++) {
+        q[pattern.charAt(i)] |= 1 << (pattern.length - i - 1)
+      }
+
       return q
     })()
 
@@ -53,68 +53,62 @@ module.exports = function (text, pattern, options) {
     if (best_loc != -1) {
       score_threshold = Math.min(match_bitapScore_(0, best_loc), score_threshold)
     }
+  }
 
-    // Initialise the bit arrays.
-    var matchmask = 1 << (pattern.length - 1);
-    best_loc = -1;
+  // Initialise the bit arrays.
+  var matchmask = 1 << (pattern.length - 1)
+  best_loc = -1
 
-    var bin_min, bin_mid;
-    var bin_max = pattern.length + text.length;
-    var last_rd;
-    for (var d = 0, dl = pattern.length; d < dl; d++) {
-        // Scan for the best match; each iteration allows for one more error.
-        // Run a binary search to determine how far from 'loc' we can stray at this
-        // error level.
-        bin_min = 0;
-        bin_mid = bin_max;
-        while (bin_min < bin_mid) {
-            if (match_bitapScore_(d, loc + bin_mid) <= score_threshold) {
-                bin_min = bin_mid;
-            } else {
-                bin_max = bin_mid;
-            }
-            bin_mid = Math.floor((bin_max - bin_min) / 2 + bin_min);
-        }
-        // Use the result from this iteration as the maximum for the next.
-        bin_max = bin_mid;
-        var start = Math.max(1, loc - bin_mid + 1);
-        var finish = Math.min(loc + bin_mid, text.length) + dl;
+  var bin_min, bin_mid
+  var bin_max = pattern.length + text.length
+  var last_rd
+  for (var d = 0; d < pattern.length; d++) {
+    // Scan for the best match; each iteration allows for one more error.
+    // Run a binary search to determine how far from 'loc' we can stray at this
+    // error level.
+    bin_min = 0
+    bin_mid = bin_max
+    while (bin_min < bin_mid) {
+      if (match_bitapScore_(d, loc + bin_mid) <= score_threshold) {
+        bin_min = bin_mid
+      } else {
+        bin_max = bin_mid
+      }
+      bin_mid = Math.floor((bin_max - bin_min) / 2 + bin_min)
+    }
+    // Use the result from this iteration as the maximum for the next.
+    bin_max = bin_mid
+    var start = Math.max(1, loc - bin_mid + 1)
+    var finish = Math.min(loc + bin_mid, text.length) + pattern.length
 
-        var rd = Array(finish + 2);
-        rd[finish + 1] = (1 << d) - 1;
-        for (var j = finish; j >= start; j--) {
-            // The alphabet (s) is a sparse hash, so the following line generates
-            // warnings.
-            var charMatch = s[text.charAt(j - 1)];
-            if (d === 0) {    // First pass: exact match.
-                rd[j] = ((rd[j + 1] << 1) | 1) & charMatch;
-            } else {    // Subsequent passes: fuzzy match.
-                rd[j] = (((rd[j + 1] << 1) | 1) & charMatch) |
-                                (((last_rd[j + 1] | last_rd[j]) << 1) | 1) |
-                                last_rd[j + 1];
-            }
-            if (rd[j] & matchmask) {
-                var score = match_bitapScore_(d, j - 1);
-                // This match will almost certainly be better than any existing match.
-                // But check anyway.
-                if (score <= score_threshold) {
-                    // Told you so.
-                    score_threshold = score;
-                    best_loc = j - 1;
-                    if (best_loc > loc) {
-                        // When passing loc, don't exceed our current distance from loc.
-                        start = Math.max(1, 2 * loc - best_loc);
-                    } else {
-                        // Already passed loc, downhill from here on in.
-                        break;
-                    }
-                }
-            }
-        }
-        // No hope for a (better) match at greater error levels.
-        if (match_bitapScore_(d + 1, loc) > score_threshold) {
-            break;
-
+    var rd = Array(finish + 2)
+    rd[finish + 1] = (1 << d) - 1
+    for (var j = finish; j >= start; j--) {
+      // The alphabet (s) is a sparse hash, so the following line generates
+      // warnings.
+      var charMatch = s[text.charAt(j - 1)]
+      if (d === 0) {
+        // First pass: exact match.
+        rd[j] = ((rd[j + 1] << 1) | 1) & charMatch
+      } else {
+        // Subsequent passes: fuzzy match.
+        rd[j] = (((rd[j + 1] << 1) | 1) & charMatch) | (((last_rd[j + 1] | last_rd[j]) << 1) | 1) | last_rd[j + 1]
+      }
+      if (rd[j] & matchmask) {
+        var score = match_bitapScore_(d, j - 1)
+        // This match will almost certainly be better than any existing match.
+        // But check anyway.
+        if (score <= score_threshold) {
+          // Told you so.
+          score_threshold = score
+          best_loc = j - 1
+          if (best_loc > loc) {
+            // When passing loc, don't exceed our current distance from loc.
+            start = Math.max(1, 2 * loc - best_loc)
+          } else {
+            // Already passed loc, downhill from here on in.
+            break
+          }
         }
       }
     }
