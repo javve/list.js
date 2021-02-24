fd9e8271bdfd848d5e9faf8a7cb7dcbcc43f7a16
Sebastiano Degan <sebastiano@localhost.localdomain>
2021-02-24 21:21:20 +0100

    removed useless regex characters escape

--- src/search.js
+++ src/search.js
@@ -28,7 +28,6 @@ module.exports = function (list) {
     },
     setSearchString: function (s) {
       s = list.utils.toString(s).toLowerCase()
-      s = s.replace(/[-[\]{}()*+?.,\\^$|#]/g, '\\$&') // Escape regular expression characters
       searchString = s
     },
     toArray: function (values) {
