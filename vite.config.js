58707072878755322d01a6511d3b080643e743c4
Jonny Strömberg <jonny.stromberg@gmail.com>
2025-04-25 14:35:57 +0900

    Update build script

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,17 @@ export default defineConfig({
     lib: {
       entry: 'src/index.js',
       name: 'List',
-      fileName: (format) => (format === 'umd' ? 'list.js' : 'list.min.js'),
-      formats: ['umd'],
+      formats: ['umd', 'esm', 'cjs'],
+      fileName: (format) => (format === 'umd' ? 'list.js' : `list.${format}.js`),
     },
     sourcemap: true,
     rollupOptions: {
       output: {
-        entryFileNames: (chunkInfo) => (chunkInfo.name === 'list' ? 'list.js' : 'list.min.js'),
         plugins: [
           terser({
             format: {
               comments: /^! List.js v.*/,
             },
-            include: [/\.min\.js$/],
             mangle: true,
           }),
         ],
